c3498095584227306f95b86e08f66bc9fac9b376
Max Gavrilyuk <max.gavr.gmi@gmail.com>
2024-09-06 22:57:45 +0300

    Setup Godot LSP

--- init.lua
+++ init.lua
@@ -330,7 +330,7 @@ vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'vimdoc', 'vim', 'gdscript' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
